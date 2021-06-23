f6f086e7aa1f1f2de6bda2e2dcff6f5b6dd4ad84
MarianoP26 <84840897+MarianoP26@users.noreply.github.com>
2021-06-23 10:57:36 +0200

    Update index.js

--- learning-react/src/index.js
+++ learning-react/src/index.js
@@ -3,16 +3,19 @@ import ReactDom from 'react-dom';
 
 // CSS
 import './index.css';
+
 // setup vars
 const firstBook = {
   img: 'https://images-na.ssl-images-amazon.com/images/I/71ytOPkFQrL._AC_UL200_SR200,200_.jpg',
   title: 'Como cocinar pollo',
   author: 'Karlos Arguiniano',
+  price: 7.50,
 }
 const secondBook = {
   img: 'https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg',
   title: 'Si los animales se dieran el beso de las buenas noches',
   author: 'Ann Whitford Paul',
+  price: 10.00,
 }
 
 //The function component can be either an arrow function or a normal one
@@ -21,8 +24,8 @@ const secondBook = {
 function BookList() {
   return (
   <section className="booklist">
-    <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
-    <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
+    <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} price={firstBook.price}/>
+    <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} price={secondBook.price}/>
   </section>
   )
 }
@@ -33,6 +36,7 @@ const Book = (props) => {
     <img src={props.img} alt="" />
     <h1>{props.title}</h1>
     <h4>{props.author}</h4>
+    <h3>{props.price}€</h3>
   </article>
   )
 }
